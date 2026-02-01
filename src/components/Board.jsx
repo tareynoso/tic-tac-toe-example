6abc16b3bba104288523d062b2b9f2d82c92812b
Tomás Reynoso <tareynoso@gmail.com>
2026-02-01 20:47:09 -0300

    integrate hardcoding square into two loops

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -13,6 +13,7 @@ export default function Board({ xIsNext, squares, onPlay }) {
     }
     onPlay(nextSquares);
   }
+  console.log(squares);
 
   const winner = calculateWinner(squares);
   const status = winner
@@ -45,21 +46,24 @@ export default function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {Array(3)
+        .fill(null)
+        .map((_, index) => (
+          <div key={index} className="board-row">
+            {Array(3)
+              .fill(null)
+              .map((_, j) => {
+                const squareIndex = index * 3 + j;
+                return (
+                  <Square
+                    key={squareIndex}
+                    value={squares[squareIndex]}
+                    onSquareClick={() => handleClick(squareIndex)}
+                  />
+                );
+              })}
+          </div>
+        ))}
     </>
   );
 }
